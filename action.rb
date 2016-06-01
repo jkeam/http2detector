e3fa84b3269149614feab10176f3f9eea4b75a5d
Jonathan Keam <jpkeam@gmail.com>
2016-06-01 14:50:13 -0400

    changed to call curl using remote service

--- action.rb
+++ action.rb
@@ -1,4 +1,6 @@
 require 'erb'
+require 'net/http'
+require 'json'
 
 class Action
   attr_reader :headers, :body, :request
@@ -43,11 +45,20 @@ class Action
   end
 
   def version(website)
-    to_return = nil
-    protocol_line = `curl --http2 -I https://"#{website}"`
-    lines = protocol_line.split("\n") if protocol_line
-    to_return = lines[0].split(' ')[0] if lines[0] 
-    to_return
+    # calls curl locallly
+    # to_return = nil
+    # protocol_line = `curl --http2 -I https://"#{website}"`
+    # lines = protocol_line.split("\n") if protocol_line
+    # to_return = lines[0].split(' ')[0] if lines[0] 
+    # to_return
+
+    uri = URI("http://162.243.195.43:8080/?website=https://#{website}")
+    resp = JSON.parse Net::HTTP.get(uri)
+    if resp['methods']
+      resp['methods'].join(',')
+    else
+      ''
+    end
   end
 
   def validate(website)
